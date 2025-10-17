4c30109c3b758b9ca0a7a2d3b3178fd50ab6d22f
Camille Leclerc <camille.leclerc@ymail.com>
2025-10-17 16:23:27 +0200

    Add documentation for dataset 'pred_win'

---
+++ R/pred_win.R
@@ -0,0 +1,16 @@
+#'  Predation windows
+#'
+#' Interspecific variation in size dependent piscivory  that was used to model fish-fish interactions in trophic networks.
+#'
+#' @format Un data frame avec 61 lignes et 9 colonnes :
+#' \describe{
+#'   \item{species_code}{Species code of the species}
+#'   \item{alpha_min}{xx}
+#'   \item{beta_min}{xx}
+#'   \item{alpha_max}{Description de la variable 2}
+#'   \item{beta_max}{Description de la variable 1}
+#'   \item{alpha_mean}{Description de la variable 2}
+#'   \item{beta_mean}{Description de la variable 1}
+#' }
+#' @source Bonnaffé, Danet et al. (2021). Comparison of size-structured and species-level trophic networks reveals antagonistic effects of temperature on vertical trophic diversity at the population and species level. Oikos, 130, 1297–1309. https://doi.org/10.1111/oik.08173
+"pred_win"
