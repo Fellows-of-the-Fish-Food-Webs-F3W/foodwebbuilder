8d146fe8c12c57adc4973717b70dcef807fcd5c2
zool1262 <zool1262@BIO-M-DT-02702.local>
2025-11-10 17:06:23 +0000

    Add dev-tmp folder and build metaweb code

---
+++ dev-tmp/metaweb-build/main.R
@@ -0,0 +1,346 @@
+############
+## main.R ##
+############
+
+## Goal: Development file for the build_metaweb function
+
+##############
+## INITIATE ##
+##############
+
+## Load packages
+library("foodwebbuilder")
+
+## Load functions
+source("metaweb_build.R")
+
+#
+###
+
+####################
+## INITIAL CHECKS ##
+####################
+
+## Individual body length measurements
+data(ind_measure)
+head(ind_measure)
+nrow(ind_measure)
+
+## Fish ontogenetic diet shifts
+data(fish_diet_shift)
+head(fish_diet_shift)
+nrow(fish_diet_shift)
+
+## Resource ontogenetic diet shifts
+data(resource_diet_shift)
+head(resource_diet_shift)
+nrow(resource_diet_shift)
+
+## Fish predation windows
+data(pred_win)
+head(pred_win)
+nrow(pred_win)
+
+#
+###
+
+######################
+## GLOBAL VARIABLES ##
+######################
+
+NUM_CLASSES = 3
+
+#
+###
+
+#####################################
+## CHECK FOR MISSING DATA AND TRIM ##
+#####################################
+
+## All species codes
+species_code_samples = unique(ind_measure$species_code)
+species_code_ods = unique(fish_diet_shift$species_code)
+species_code_pred = unique(pred_win$species_code)
+
+## Check samples -> ods
+check_1 = is.na(match(species_code_samples, species_code_ods))*1
+check_2 = is.na(match(species_code_samples, species_code_pred))*1
+missing = species_code_samples[(check_1 | check_2)]
+
+## Remove
+for(missing_ in missing){
+  s = which(ind_measure$species_code == missing_)
+  ind_measure = ind_measure[-s,]
+}
+
+#
+###
+
+##########################
+## COMPUTE SIZE CLASSES ##
+##########################
+
+## All species codes
+species_code = unique(ind_measure$species_code)
+
+## Get body length classes
+tab_size_classes = NULL
+for (species_code_ in species_code){
+  
+  ## Iterator
+  print(species_code_)
+  
+  ## Subset data
+  s = ind_measure$species_code == species_code_
+  ind_measure_ = ind_measure[s,]
+  print(nrow(ind_measure_))
+  
+  ## Compute max length
+  size_max_ = max(ind_measure_$size)
+  
+  ## Divide in size classes
+  size_classes_ = seq(0, size_max_, size_max_/NUM_CLASSES)
+  print(size_classes_)
+  
+  ## Collect
+  tab_size_classes = rbind(tab_size_classes, c(species_code_, size_classes_))
+  
+}
+tab_size_classes = data.frame(tab_size_classes)
+colnames(tab_size_classes) = c("species_code", "lower_bound", paste("upper_bound", 1:NUM_CLASSES, sep="_"))
+
+#       
+###
+
+############################
+## DEFINE TROPHIC SPECIES ##
+############################
+
+## Flatten
+lb_size_classes = as.numeric(t(as.matrix(tab_size_classes[,-c(1,ncol(tab_size_classes))])))
+ub_size_classes = as.numeric(t(as.matrix(tab_size_classes[,-c(1,2)])))
+mp_size_classes = 0.5 * (ub_size_classes + lb_size_classes)
+trophic_species_code = paste(rep(species_code, 1, each=NUM_CLASSES), 1:NUM_CLASSES, sep="_")
+
+#
+###
+
+###############################
+## COMPUTE PREY SIZE CLASSES ##
+###############################
+
+## Subset rows in predation window file
+species_code_ = tab_size_classes$species_code
+s = match(species_code_, pred_win$species_code)
+pred_win_ = pred_win[s,]
+
+## Check for NA
+missing = species_code[is.na(pred_win_$species_code)]
+
+## Compute class midpoints
+tab_size_classes_midpoints = NULL
+for (j in 2:(ncol(tab_size_classes)-1))
+{
+  tab_size_classes_midpoints_ = 0.5 * (as.numeric(tab_size_classes[,j+1]) + as.numeric(tab_size_classes[,j]))
+  tab_size_classes_midpoints = cbind(tab_size_classes_midpoints, tab_size_classes_midpoints_)
+}
+
+## Compute prey lower bound and upper bound
+lb_prey = tab_size_classes_midpoints * pred_win_$beta_min
+ub_prey = tab_size_classes_midpoints * pred_win_$beta_max
+
+#
+###
+
+####################################################
+## DETERMINE ALL POTENTIAL FISH-FISH INTERACTIONS ##
+####################################################
+
+## Vectorise 
+vec_size_classes_midpoints = as.numeric(t(tab_size_classes_midpoints))
+vec_lb_prey = as.numeric(t(lb_prey))
+vec_ub_prey = as.numeric(t(ub_prey))
+
+## Build fish-fish interaction matrix
+ff_interactions = NULL
+for (i in 1:length(vec_size_classes_midpoints)){
+  ff_interactions_ = (vec_size_classes_midpoints >= vec_lb_prey[i]) * (vec_size_classes_midpoints < vec_ub_prey[i])  
+  ff_interactions = rbind(ff_interactions, ff_interactions_)
+}
+
+## Format matrix
+trophic_species_code = paste(rep(species_code, 1, each=NUM_CLASSES), 1:NUM_CLASSES, sep="_")
+colnames(ff_interactions) = trophic_species_code
+rownames(ff_interactions) = trophic_species_code
+ff_interactions = t(ff_interactions) # i --> j (row i is consumed by column j)
+dim(ff_interactions)
+
+#
+###
+
+############################
+## FISH-FISH INTERACTIONS ##
+############################
+
+## For each trophic species check piscivory status
+piscivory = NULL
+for (i in 1:length(trophic_species_code)){
+  
+  ## Subset ODS data
+  species_code_ = unlist(strsplit(trophic_species_code[i],"_"))[1]
+  s = fish_diet_shift$species_code == species_code_
+  fish_diet_shift_ = fish_diet_shift[s,]
+  
+  ## Find match
+  check = (vec_size_classes_midpoints[i] >= fish_diet_shift_$size_min) & (vec_size_classes_midpoints[i] < fish_diet_shift_$size_max)
+  
+  ## Check status
+  piscivory_ = fish_diet_shift_[check, "fish"]
+  piscivory = c(piscivory, piscivory_)
+  
+}
+
+## Apply filter
+ff_interactions = ff_interactions %*% diag(piscivory)
+
+## Format
+colnames(ff_interactions) = trophic_species_code
+rownames(ff_interactions) = trophic_species_code
+
+#
+###
+
+################################
+## RESOURCE-FISH INTERACTIONS ##
+################################
+
+## For each trophic species check piscivory status
+rf_interactions = NULL
+selected_resources = c("det", "biof", "phytob", "macroph", "phytopl", "zoopl", "zoob")
+for (i in 1:length(trophic_species_code)){
+  
+  ## Subset ODS data
+  species_code_ = unlist(strsplit(trophic_species_code[i],"_"))[1]
+  s = fish_diet_shift$species_code == species_code_
+  fish_diet_shift_ = fish_diet_shift[s,]
+  
+  ## Find match
+  check = (vec_size_classes_midpoints[i] >= fish_diet_shift_$size_min) & (vec_size_classes_midpoints[i] < fish_diet_shift_$size_max)
+  
+  ## Check status
+  rf_interactions_ = fish_diet_shift_[check, ]
+  rf_interactions = rbind(rf_interactions, rf_interactions_)
+  
+}
+
+## Format
+rf_interactions = t(rf_interactions[,selected_resources])
+colnames(rf_interactions) = trophic_species_code
+rownames(rf_interactions) = selected_resources
+
+#
+###
+
+######################
+## ASSEMBLE METAWEB ##
+######################
+
+## Resource-resource interactions
+rr_interactions = resource_diet_shift[, selected_resources]
+rownames(rr_interactions) = selected_resources
+colnames(rr_interactions) = selected_resources
+dim(rr_interactions)
+
+## fish-resource interactions
+fr_interactions = matrix(0, ncol=ncol(rr_interactions), nrow=ncol(ff_interactions))
+rownames(fr_interactions) = trophic_species_code
+colnames(fr_interactions) = selected_resources
+
+## rr
+metaweb = cbind(
+  rbind(ff_interactions, rf_interactions),
+  rbind(fr_interactions, rr_interactions)
+)
+
+## 
+image(as.matrix(metaweb))
+
+#
+###
+
+#####################
+## LOCAL FOOD WEBS ##
+#####################
+
+## Random fish sample
+num_samples = 30
+u = round(runif(num_samples,0,1) * nrow(ind_measure))
+ind_measure_local = ind_measure[u,]
+
+## Check presence
+check = NULL
+for (i in 1:nrow(ind_measure_local)){
+  
+  ## Check presence
+  check_ = (
+    grepl(pattern = ind_measure_local$species_code[i], x = trophic_species_code) & 
+    (ind_measure_local$size[i] >= lb_size_classes) & 
+    (ind_measure_local$size[i] < ub_size_classes)  
+    )*1
+  
+  ## Collect
+  check = rbind(check, check_)
+}
+
+## Collapse
+check = apply(check, 2, sum)
+cbind(trophic_species_code, check)
+s = which(check > 0)
+trophic_species_code_subset = c(trophic_species_code[s], selected_resources)
+
+## Subset metaweb
+metaweb[trophic_species_code_subset, trophic_species_code_subset]
+
+#
+###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
