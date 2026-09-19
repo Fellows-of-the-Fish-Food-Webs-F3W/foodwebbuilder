bc753684f2627899cd2b605c5f5f3ae036941459
CamilleLeclerc <camille.leclerc@ymail.com>
2026-09-19 18:42:12 +0200

    Clarify predation window parameter definitions in vignette

--- R/pred_win.R
+++ R/pred_win.R
@@ -7,14 +7,12 @@
 #' @format A data frame with 61 rows and 9 columns:
 #' \describe{
 #'   \item{species_code}{Three-letter species code.}
-#'   \item{alpha_min}{Lower slope coefficient defining
-#'   the minimum predation window.}
-#'   \item{beta_min}{Intercept defining the minimum predation window.}
-#'   \item{alpha_max}{Upper slope coefficient defining
-#'   the maximum predation window.}
-#'   \item{beta_max}{Intercept defining the maximum predation window.}
-#'   \item{alpha_mean}{Slope coefficient for the mean predation window.}
-#'   \item{beta_mean}{Intercept for the mean predation window.}
+#'   \item{alpha_min}{Intercept of the lower boundary of the predation window.}
+#'   \item{beta_min}{Slope of the lower boundary of the predation window.}
+#'   \item{alpha_max}{Intercept of the upper boundary of the predation window.}
+#'   \item{beta_max}{Slope of the upper boundary of the predation window.}
+#'   \item{alpha_mean}{SIntercept of the mean predator–prey body-size relationship.}
+#'   \item{beta_mean}{Slope of the mean predator–prey body-size relationship.}
 #' }
 #'
 #' @source Bonnaffé, C., Danet, A., et al. (2021).
